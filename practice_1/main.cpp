76c2e0308f5a121e68aa8a800e25bacbab7b1a7e
Liromanz <teo.tb@mail.ru>
2024-02-25 23:29:50 +0300

    первый практос

---
+++ practice_1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int first = 0;
+    int second = 1;
+    int third = first + second;
+    int fourth = second + third;
+    int fifth = third + fourth;
+
+    cout<< first << endl;
+    cout<< second << endl;
+    cout<< third << endl;
+    cout<< fourth << endl;
+    cout<< fifth << endl;
+}
